5563b664ee6088a8c554bf6cec57e2090cb135a2
Santiago Vega <svegaromero@gmail.com>
2020-01-24 12:02:15 -0500

    Procfile

--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -10,6 +10,17 @@ public class App
 {
     public static void main( String[] args )
     {
+        port(getPort());
+     
+        
         get("/hello", (req, res) -> "Hello Heroku");
+    }
+    
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }        
+           
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)    }
     }
 }
